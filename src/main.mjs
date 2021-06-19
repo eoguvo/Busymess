b5f3e31eb08821ef39894c8353d89f92bdb48a6c
Gustav0 Henrique <gustavooliveiraitu63@gmail.com>
2021-06-18 23:26:48 -0300

    :sparkles: drag and drop :sparkles:

--- src/main.mjs
+++ src/main.mjs
@@ -1,1 +1,45 @@
-console.log("foi o/")
+import { mock } from './mock';
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const todos = JSON.parse(localStorage.getItem('boards')) || mock || [];
+const boards = $$(".board ul")
+
+
+
+boards.forEach(el => {
+  new Sortable(el, {
+    group: 'boards',
+    animation: 150,
+    onEnd: (ev) =>{
+      const { item, target, to, oldIndex, newIndex } = ev;
+      console.log(ev, todos[to.id]);
+      todos[target.id].items.splice(oldIndex, 1);
+      todos[to.id].items.splice(newIndex, 0, { description: item.innerText });
+      save();
+    }
+  });
+});
+
+function save() {
+  localStorage.setItem('boards', JSON.stringify(todos));
+}
+
+function render() {
+  boards.forEach((board, i)=>{
+    board.innerHTML = ""
+    todos[i].items.forEach(({ description })=>{
+      board.innerHTML += template(description);
+    })
+  })
+}
+
+const template = (text) => `
+<li>
+  <span class="category"></span>
+  <p class="description">${text}</p>
+</li>
+`
+
+render();
---
+++ src/mock.js
@@ -0,0 +1,23 @@
+export const mock = [
+  {
+    title: "todo",
+    items: [
+      { "description": "fazer funcionar e pa" }
+    ]
+  },
+  {
+    title: "doing", 
+    items: [
+      { "description": "estudando e pa" },
+      { "description": "treininho e pa" }
+    ]
+  },
+  {
+    title: "done", 
+    items: [
+      { "description": "os estilos e pa" },
+      { "description": "os side project e pa" },
+      { "description": "lorem ipsum dolor sit amet e pa" }
+    ]
+  }
+]
