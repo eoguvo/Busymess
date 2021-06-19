5303611630e89a219263b1f038c81ae5e59e0d78
Gustavo-Henrique-br <62659139+Gustavo-Henrique-br@users.noreply.github.com>
2021-06-19 02:27:26 +0000

    Deploying to gh-pages from @ Gustavo-Henrique-br/Busymess@b5f3e31eb08821ef39894c8353d89f92bdb48a6c 🚀

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,7 @@
-console.log("foi o/");
+var s=[{title:"todo",items:[{description:"fazer funcionar e pa"}]},{title:"doing",items:[{description:"estudando e pa"},{description:"treininho e pa"}]},{title:"done",items:[{description:"os estilos e pa"},{description:"os side project e pa"},{description:"lorem ipsum dolor sit amet e pa"}]}];var b=document.querySelector.bind(document),p=document.querySelectorAll.bind(document),o=JSON.parse(localStorage.getItem("boards"))||s||[],r=p(".board ul");r.forEach(e=>{new Sortable(e,{group:"boards",animation:150,onEnd:t=>{let{item:i,target:c,to:n,oldIndex:d,newIndex:a}=t;console.log(t,o[n.id]),o[c.id].items.splice(d,1),o[n.id].items.splice(a,0,{description:i.innerText}),l()}})});function l(){localStorage.setItem("boards",JSON.stringify(o))}function u(){r.forEach((e,t)=>{e.innerHTML="",o[t].items.forEach(({description:i})=>{e.innerHTML+=m(i)})})}var m=e=>`
+<li>
+  <span class="category"></span>
+  <p class="description">${e}</p>
+</li>
+`;u();
 //# sourceMappingURL=main.js.map
